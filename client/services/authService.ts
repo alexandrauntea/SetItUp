12a7372b4ad6b5551afc5cbb7e376f87d632899e
stefan <stefan_petris@yahoo.com>
2026-08-03 12:42:17 +0300

    sprint 1

---
+++ client/services/authService.ts
@@ -0,0 +1,46 @@
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  type Unsubscribe,
+  type User,
+} from 'firebase/auth';
+
+import { auth } from '@/services/firebase';
+
+export async function registerUser(
+  email: string,
+  password: string
+): Promise<User> {
+  const userCredential = await createUserWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+  return userCredential.user;
+}
+
+export async function loginUser(
+  email: string,
+  password: string
+): Promise<User> {
+  const userCredential = await signInWithEmailAndPassword(
+    auth,
+    email,
+    password
+  );
+
+  return userCredential.user;
+}
+
+export async function logoutUser(): Promise<void> {
+  await signOut(auth);
+}
+
+export function subscribeToAuthChanges(
+  callback: (user: User | null) => void
+): Unsubscribe {
+  return onAuthStateChanged(auth, callback);
+}
