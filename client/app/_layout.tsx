c8156ae093a5d3bbbdc5118552a1e67576af55b6
Alexandra <alexandra.untea@stud.acs.upb.ro>
2026-08-03 12:31:36 +0300

    layout

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,25 +1,57 @@
 import '@/services/firebase';
+import { useEffect } from 'react';
+import {ActivityIndicator, View} from 'react-native';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, useRouter, useSegments } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
+import { AuthProvider, useAuth } from '../contexts/AuthContext';
+
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
-export default function RootLayout() {
+function RootLayoutNav() {
   const colorScheme = useColorScheme();
-
+  const {isAuthenticated, isLoading} = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+  useEffect(() => {
+    if (isLoading) return; 
+    const inAuthGroyp = segments[0] === '(auth)';
+    if (!isAuthenticated && !inAuthGroyp) {
+      router.replace('/(auth)/login');
+    } else if (isAuthenticated && inAuthGroyp) {
+      router.replace('/(tabs)');
+    }
+  }, [isAuthenticated, isLoading, segments]
+  );
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name = "(auth" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
+  );
+}
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <RootLayoutNav />
+    </AuthProvider>
   );
 }
