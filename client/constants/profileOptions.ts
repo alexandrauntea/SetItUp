ebd3add6bf6facd79eb7282eddfa08befea6f8cb
Andrei Barbuceanu <andreibarbuceanu@Andreis-MacBook-Air-3.local>
2026-08-12 23:37:35 +0300

    Group profile interests by category

--- client/constants/profileOptions.ts
+++ client/constants/profileOptions.ts
@@ -12,12 +12,70 @@ export const GENDER_OPTIONS: GenderOption[] = [
 ];
 
 export const INTEREST_OPTIONS = [
-  "Tehnologie",
-  "Muzică",
-  "Călătorii",
-  "Sport",
-  "Filme",
-  "Gaming",
-  "Lectură",
+  // Artă, media și activități creative
   "Artă",
+  "Design",
+  "Fotografie",
+  "Modă",
+  "Scris",
+  "Teatru",
+  "Filme",
+  "Seriale",
+  "Documentare",
+  "Anime",
+  "Lectură",
+  "Podcasturi",
+  "Gaming",
+  "Jocuri de societate",
+
+  // Muzică și ieșiri
+  "Muzică",
+  "Concerte",
+  "Festivaluri",
+  "Dans",
+
+  // Călătorii și activități în aer liber
+  "Călătorii",
+  "Natură",
+  "Drumeții",
+  "Camping",
+  "Plajă",
+
+  // Sport și mișcare
+  "Sport",
+  "Fitness",
+  "Alergare",
+  "Ciclism",
+  "Înot",
+  "Fotbal",
+  "Baschet",
+  "Tenis",
+  "Sporturi de iarnă",
+  "Yoga",
+
+  // Mâncare și băuturi
+  "Gătit",
+  "Restaurante",
+  "Cafea",
+  "Vin",
+
+  // Tehnologie, cunoaștere și carieră
+  "Tehnologie",
+  "Știință",
+  "Istorie",
+  "Limbi străine",
+  "Antreprenoriat",
+
+  // Animale și plante
+  "Animale",
+  "Câini",
+  "Pisici",
+  "Plante",
+
+  // Stare de bine și valori
+  "Dezvoltare personală",
+  "Meditație",
+  "Wellness",
+  "Ecologie",
+  "Voluntariat",
 ];
